20e852573466a5f3ade0d3756d490ac5edbb5373
Rafhael <rafhaelalvesprado@gmail.com>
2024-12-18 15:46:08 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$('#telefone').mask('(00) 00000-0000', {
+})
+
+$('form').validate({
+    rules: {
+        nome: {
+            required: true
+        },
+        email: {
+            required: true
+        },
+        telefone: {
+            required: true
+        },
+        mensagem: {
+            required: true
+        },
+    },
+    submitHandler: function (form) {
+        alert("Obrigado pela sua mensagem! Logo mais retornaremos seu contato.");
+        form.reset();
+    },
+})
